456a23f6c3fb1e5285d474546280aabb998babf8
Paul Logston <paul.logston@gmail.com>
2022-07-25 22:43:09 -0700

    Add Prometheus instrumentation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # libre-computer-ping
+
+### Build
+```
+go build -o ping main.go
+```
+
+### Usage
+```
+./ping 5s 1.1.1.1:443 1.0.0.1:443 8.8.8.8:443 8.8.4.4:443 208.67.222.222:443 208.67.220.220:443
+```
+
+Access Prometheus metrics at `127.0.0.1:2112/metrics`
--- main.go
+++ main.go
@@ -3,12 +3,42 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
 	timeout = time.Duration(1 * time.Second)
+)
+
+var (
+	connLatency = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "latency",
+			Help: "Connection latency (in ms) to address.",
+		},
+		[]string{"host"},
+	)
+	connAttempts = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "attempts",
+			Help: "Attempted connections to address.",
+		},
+		[]string{"host"},
+	)
+	connSuccesses = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "successes",
+			Help: "Successful connections to address.",
+		},
+		[]string{"host"},
+	)
 )
 
 func main() {
@@ -17,8 +47,8 @@ func main() {
 		help()
 		panic(err)
 	}
-
-	host, port, duration := args[0], args[1], args[2]
+	duration := args[0]
+	addresses := args[1:]
 
 	d, err := time.ParseDuration(duration)
 	if err != nil {
@@ -26,32 +56,52 @@ func main() {
 		panic(err)
 	}
 
-	for {
-		latency, err := connect(host, port, d)
-		if err != nil {
-			fmt.Printf("connection error: %v\n", err)
-		} else {
-			fmt.Printf("connected in %dms\n", latency.Milliseconds())
-		}
-		time.Sleep(d)
+	for _, address := range addresses {
+		go loop(address, d)
 	}
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
 
 func validateArgs(args []string) error {
-	if len(args) != 3 {
+	if len(args) <= 1 {
 		return fmt.Errorf("invalid args")
+	}
+
+	for _, address := range args[1:] {
+		if !strings.Contains(address, ":") {
+			return fmt.Errorf("address %q is not valid", address)
+		}
 	}
 
 	return nil
 }
 
 func help() {
-	fmt.Println("./ping <host> <port> <interval>")
+	fmt.Println("./ping <interval> <host>:<port> <host>:<port>...")
 }
 
-func connect(host, port string, timeout time.Duration) (*time.Duration, error) {
+func loop(address string, d time.Duration) {
+
+	for {
+		connAttempts.WithLabelValues(address).Inc()
+		latency, err := connect(address, d)
+		if err != nil {
+			fmt.Printf("connection error: %v\n", err)
+		} else {
+			connSuccesses.WithLabelValues(address).Inc()
+			connLatency.WithLabelValues(address).Set(float64(latency.Milliseconds()))
+			fmt.Printf("connected to %s in %dms\n", address, latency.Milliseconds())
+		}
+
+		time.Sleep(d)
+	}
+}
+
+func connect(address string, timeout time.Duration) (*time.Duration, error) {
 	s := time.Now()
-	c, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", host, port), timeout)
+	c, err := net.DialTimeout("tcp", address, timeout)
 	if err != nil {
 		return nil, err
 	}
